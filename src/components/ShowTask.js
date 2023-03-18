21511d5351ea1b9252ce6b6328764a4c211cf182
Sultan-home <sultat.orunov@mail.ru>
2023-03-18 18:21:02 +0500

    added task deletion

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,26 +1,30 @@
 export const ShowTask = ({taskList, setTaskList}) => {
+    const handleDelete = (id) => {
+        const newTaskList = taskList.filter(taskItem => taskItem.id !== id)
+        setTaskList(newTaskList)
+    }
 
-  return (
-    <section className="showTask">
-        <div className="head">
-            <div>
-                <span className="title">Todo</span>
-                <span className="count">{taskList.length}</span>
+    return (
+        <section className="showTask">
+            <div className="head">
+                <div>
+                    <span className="title">Todo</span>
+                    <span className="count">{taskList.length}</span>
+                </div>
+                <button className="clearAll">Clear All</button>
             </div>
-            <button className="clearAll">Clear All</button>
-        </div>
-        <ul>
-            { taskList.map((taskItem) => (
-                <li key={taskItem.id}>
-                    <p>
-                        <span className="name">{taskItem.name}</span>
-                        <span className="time">{taskItem.time}</span>
-                    </p>
-                    <i className="bi bi-pencil-square"></i>
-                    <i className="bi bi-trash"></i>
-                </li>
-            )) }            
-        </ul>
-    </section>
-  )
+            <ul>
+                {taskList.map((taskItem) => (
+                    <li key={taskItem.id}>
+                        <p>
+                            <span className="name">{taskItem.name}</span>
+                            <span className="time">{taskItem.time}</span>
+                        </p>
+                        <i className="bi bi-pencil-square"></i>
+                        <i onClick={() => handleDelete(taskItem.id)} className="bi bi-trash"></i>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    )
 }
